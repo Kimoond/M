d73a9074369ffdfbe20957d20007848e9651f1ac
Kmh0122** <qmmun543@gmail.com>
2023-06-09 10:39:03 +0900

    0609

---
+++ app/src/main/java/com/example/exam_shop2022/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.exam_shop2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity  {
+    ImageView img_product_main;
+    EditText edit_count;
+    TextView txt_price, txt_delivery, txt_pay;
+    CheckBox chk_agree;
+
+    int val_price;                  // 제품의 총 가격(배송비 미포함)
+    int val_delivery;               // 배송비 ( 제품 총 가격이 10000원 이상이면 0원, 미만이면 2500원)
+    int val_pay;                    // 총 결제 금액
+    int selected_product=2000;      // 선택한 제품 한개의 가격
+    int selected_count;             // 선택한 수량
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img_product_main =findViewById(R.id.img_product_main);
+        edit_count =findViewById(R.id.edit_count);
+        txt_price=findViewById(R.id.txt_price);
+        txt_delivery =findViewById(R.id.txt_delivery);
+        txt_pay =findViewById(R.id.txt_pay);
+        chk_agree = findViewById(R.id.chk_agree);
+
+        findViewById(R.id.radio1).setOnClickListener(this);
+        findViewById(R.id.radio2).setOnClickListener(this);
+        findViewById(R.id.radio3).setOnClickListener(this);
+        findViewById(R.id.radio4).setOnClickListener(this);
+        findViewById(R.id.btn_minus).setOnClickListener(this);
+        findViewById(R.id.btn_plus).setOnClickListener(this);
+        findViewById(R.id.txt_pay).setOnClickListener(this);
+
+
+
+
+
+    }
+
+}
